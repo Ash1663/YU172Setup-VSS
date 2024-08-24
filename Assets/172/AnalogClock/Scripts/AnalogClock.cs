2deb0f878ef3828ee0b9de8b58eb3e0090c5c8fe
Ash1663 <57282474+Ash1663@users.noreply.github.com>
2024-08-25 01:27:22 +0900

    YU172を投入

---
+++ Assets/172/AnalogClock/Scripts/AnalogClock.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UdonSharp;
+using UnityEngine;
+
+namespace nekobako {
+	public class AnalogClock : UdonSharpBehaviour {
+		[SerializeField]
+		private Transform HourHand = null;
+
+		[SerializeField]
+		private Transform MinuteHand = null;
+
+		[SerializeField]
+		private Transform SecondHand = null;
+
+
+		private void Update() {
+			float milliseconds = (float)DateTime.Now.TimeOfDay.TotalMilliseconds;
+
+			this.HourHand.localRotation = Quaternion.AngleAxis(milliseconds / 1000.0f / 60.0f / 60.0f / 12.0f * 360.0f, Vector3.forward);
+			this.MinuteHand.localRotation = Quaternion.AngleAxis(milliseconds / 1000.0f / 60.0f / 60.0f * 360.0f, Vector3.forward);
+			this.SecondHand.localRotation = Quaternion.AngleAxis(milliseconds / 1000.0f / 60.0f * 360.0f, Vector3.forward);
+		}
+	}
+}
---
+++ Assets/172/Udon/TurnCoordinator.cs
@@ -0,0 +1,30 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace Virtual172
+{
+    public class TurnCoordinator : UdonSharpBehaviour
+    {
+        public float angleScale = 1.0f;
+        public float clampAngle = 20.0f;
+        public Vector3 axis = Vector3.forward;
+
+        private Rigidbody rigidbody;
+
+        private void Start()
+        {
+            rigidbody = GetComponentInParent<Rigidbody>();
+        }
+
+        private void Update()
+        {
+            transform.localRotation = Quaternion.AngleAxis(
+                Mathf.Clamp(rigidbody.angularVelocity.y * angleScale, -clampAngle, clampAngle),
+                axis
+            );
+        }
+    }
+}
